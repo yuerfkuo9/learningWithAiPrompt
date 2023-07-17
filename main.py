67b003d923ecaa5e530605c8ef398647cd03b058
caviar <ycxeva@outlook.com>
2023-07-18 00:04:29 +0800

    python day 7

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ for num in range(100,1000):
     #print(bai)
     if num == ge **3 + shi ** 3 + bai **3 :
         print('%d 是水仙花数' % (num))
-'''
+
 from random import randint
 
 money = 1000
@@ -141,3 +141,113 @@ while money > 0:
                 break
 
 print("您破产了")
+
+s1 = r'\'hello, world!\''
+s2 = r'\n\\hello, world!\\\n'
+print(s1, s2, end='')
+
+#跑马灯文字
+import os
+import time
+
+
+def main():
+    content = '北京欢迎你为你开天辟地…………'
+    while True:
+        # 清理屏幕上的输出
+        os.system('cls')  # os.system('clear')
+        print(content)
+        # 休眠200毫秒
+        time.sleep(0.2)
+        content = content[1:] + content[0]
+
+if __name__ == '__main__':
+    main()
+'''
+#设计一个函数产生指定长度的验证码，验证码由大小写字母和数字构成
+'''import random
+import string
+
+def veri_code (veri_code_length):
+    random_str = ''.join(random.sample(string.ascii_letters + string.digits,20))
+    return random_str
+print(veri_code(20))
+import random
+
+def generate_code(code_len=4):
+    """
+    生成指定长度的验证码
+
+    :param code_len: 验证码的长度(默认4个字符)
+
+    :return: 由大小写英文字母和数字构成的随机验证码
+    """
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars)-1
+    code = ''
+    for _ in range(code_len):
+        a = random.randint(0,last_pos)
+        code += all_chars[a]
+    return code
+
+print(generate_code(20))
+#设计一个函数返回给定文件名的后缀名。
+def give_file_extension(file_name):
+    index = file_name.find('.')
+    if 0< index <len(file_name) :
+        return file_name[index+1:]
+    else:
+        return ''
+
+print(give_file_extension("text."))
+
+#设计一个函数返回传入的列表中最大和第二大的元素的值
+def max_andmin_value(list):
+    max = 0
+    max2 = 0
+    for index in range(len(list)):
+        if list[index] > max :
+            max2 = max
+            max = list[index]
+        elif max2 <= list[index] < max :
+            max2 = list[index]
+    return max,max2
+print(max_andmin_value([11,22,21,10,6,7,8,9,100,98]))
+
+#计算指定的年月日是这一年的第几天。
+def is_leap(year):
+    return year % 100 != 0 and year % 4 == 0 or year % 400 == 0
+
+def set_day():
+    a = "2012年12月06日"
+    month = (31,28,31,30,31,30,31,31,30,31,30,31)
+    leap_month = (31,29,31,30,31,30,31,31,30,31,30,31)
+    year = int(a[0:4])
+    month_index = int(a[5:7])
+    day = int(a[8:10])
+    thday = 0
+    if is_leap(year) :
+        for index in range(month_index):
+            thday += leap_month[index]
+    else :
+        for index in range(month_index):
+            thday += month[index]
+    return thday + day
+#print(set_day(input("请输入年月日：")))
+print(set_day())
+'''
+def joseph (num):
+    people_num = [x for x in range(1, num+1)]
+    count = 0
+    i = 0
+    while len(people_num) > 15:
+        count += 1
+        if count == 9:
+            people_num.pop(i)
+            i -= 1
+            count = 0
+        if i >= len(people_num) :
+            i = 0
+        i += 1
+    return people_num
+print(joseph(30))
--- note.py
+++ note.py
@@ -5,3 +5,210 @@ end = ""
 "免于更改数字 %d整数占位符 %f小数占位符 %%百分号"
 
 "//是除完去除小数，%是余数"
+'''
+Python为字符串类型提供了非常丰富的运算符，我们可以使用+运算符来实现字符串的拼接，
+可以使用*运算符来重复一个字符串的内容，可以使用in和not in来判断一个字符串是否包含另外一个字符串（成员运算），
+我们也可以用[]和[:]运算符从字符串取出某个字符或某些字符（切片运算），代码如下所示。
+str2 = 'abc123456'
+# 从字符串中取出指定位置的字符(下标运算)
+print(str2[2]) # c
+# 字符串切片(从指定的开始索引到指定的结束索引)
+print(str2[2:5]) # c12
+print(str2[2:]) # c123456
+print(str2[2::2]) # c246
+print(str2[::2]) # ac246
+print(str2[::-1]) # 654321cba
+print(str2[-3:-1]) # 45
+
+str1 = 'hello, world!'
+print(len(str1)) # 13
+# 获得字符串首字母大写的拷贝
+print(str1.capitalize()) # Hello, world!
+# 获得字符串每个单词首字母大写的拷贝
+print(str1.title()) # Hello, World!
+# 获得字符串变大写后的拷贝
+print(str1.upper()) # HELLO, WORLD!
+# 从字符串中查找子串所在位置
+print(str1.find('or')) # 8
+print(str1.find('shit')) # -1
+# 与find类似但找不到子串时会引发异常
+# print(str1.index('or'))
+# print(str1.index('shit'))
+# 检查字符串是否以指定的字符串开头
+print(str1.startswith('He')) # False
+print(str1.startswith('hel')) # True
+# 检查字符串是否以指定的字符串结尾
+print(str1.endswith('!')) # True
+# 将字符串以指定的宽度居中并在两侧填充指定的字符
+print(str1.center(50, '*'))
+# 将字符串以指定的宽度靠右放置左侧填充指定的字符
+print(str1.rjust(50, ' '))
+
+str2 = 'abc123456'
+# 检查字符串是否由数字构成
+print(str2.isdigit())  # False
+# 检查字符串是否以字母构成
+print(str2.isalpha())  # False
+# 检查字符串是否以数字和字母构成
+print(str2.isalnum())  # True
+str3 = '  jackfrued@126.com '
+print(str3)
+# 获得字符串修剪左右两侧空格之后的拷贝
+print(str3.strip())
+
+a, b = 5, 10
+print('{0} * {1} = {2}'.format(a, b, a * b))
+
+a, b = 5, 10
+print(f'{a} * {b} = {a * b}')
+
+a, b = 5, 10
+print('{0} * {1} = {2}'.format(a, b, a * b))
+Python 3.6以后，格式化字符串还有更为简洁的书写方式，
+就是在字符串前加上字母f，我们可以使用下面的语法糖来简化上面的代码。
+a, b = 5, 10
+print(f'{a} * {b} = {a * b}')
+
+LIST
+list1 = [1, 3, 5, 7, 100]
+print(list1) # [1, 3, 5, 7, 100]
+# 乘号表示列表元素的重复
+list2 = ['hello'] * 3
+print(list2) # ['hello', 'hello', 'hello']
+# 计算列表长度(元素个数)
+print(len(list1)) # 5
+# 下标(索引)运算
+print(list1[0]) # 1
+print(list1[4]) # 100
+# print(list1[5])  # IndexError: list index out of range
+print(list1[-1]) # 100
+print(list1[-3]) # 5
+list1[2] = 300
+print(list1) 
+# [1, 3, 300, 7, 100]
+# 通过循环用下标遍历列表元素
+for index in range(len(list1)):
+    print(list1[index])
+# 通过for循环遍历列表元素
+for elem in list1:
+    print(elem)
+# 通过enumerate函数处理列表之后再遍历可以同时获得元素索引和值
+for index, elem in enumerate(list1):
+    print(index, elem)
+    
+list1 = [1, 3, 5, 7, 100]
+# 添加元素
+list1.append(200)
+在列表后面加
+list1.insert(1, 400)
+在列表前面加
+# 合并两个列表
+# list1.extend([1000, 2000])
+list1 += [1000, 2000]
+print(list1) # [1, 400, 3, 5, 7, 100, 200, 1000, 2000]
+print(len(list1)) # 9
+# 先通过成员运算判断元素是否在列表中，如果存在就删除该元素
+if 3 in list1:
+	list1.remove(3)
+if 1234 in list1:
+    list1.remove(1234)
+print(list1) # [1, 400, 5, 7, 100, 200, 1000, 2000]
+# 从指定的位置删除元素
+list1.pop(0)
+list1.pop(len(list1) - 1)
+print(list1) # [400, 5, 7, 100, 200, 1000]
+# 清空列表元素
+list1.clear()
+print(list1) # []
+
+fruits = ['grape', 'apple', 'strawberry', 'waxberry']
+fruits += ['pitaya', 'pear', 'mango']
+# 列表切片
+fruits2 = fruits[1:4]
+print(fruits2) # apple strawberry waxberry
+# 可以通过完整切片操作来复制列表
+fruits3 = fruits[:]
+print(fruits3) # ['grape', 'apple', 'strawberry', 'waxberry', 'pitaya', 'pear', 'mango']
+fruits4 = fruits[-3:-1]
+print(fruits4) # ['pitaya', 'pear']
+# 可以通过反向切片操作来获得倒转后的列表的拷贝
+fruits5 = fruits[::-1]
+print(fruits5) # ['mango', 'pear', 'pitaya', 'waxberry', 'strawberry', 'apple', 'grape']
+
+list1 = ['orange', 'apple', 'zoo', 'internationalization', 'blueberry']
+list2 = sorted(list1)
+# sorted函数返回列表排序后的拷贝不会修改传入的列表
+# 函数的设计就应该像sorted函数一样尽可能不产生副作用
+list3 = sorted(list1, reverse=True)
+# 通过key关键字参数指定根据字符串长度进行排序而不是默认的字母表顺序
+list4 = sorted(list1, key=len)
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+# 给列表对象发出排序消息直接在列表对象上进行排序
+list1.sort(reverse=True)
+print(list1)
+
+f = [x for x in range(1, 10)]
+print(f)
+f = [x + y for x in 'ABCDE' for y in '1234567']
+print(f)
+# 用列表的生成表达式语法创建列表容器
+# 用这种语法创建列表之后元素已经准备就绪所以需要耗费较多的内存空间
+f = [x ** 2 for x in range(1, 1000)]
+print(sys.getsizeof(f))  # 查看对象占用内存的字节数
+print(f)
+# 请注意下面的代码创建的不是一个列表而是一个生成器对象
+# 通过生成器可以获取到数据但它不占用额外的空间存储数据
+# 每次需要数据的时候就通过内部的运算得到数据(需要花费额外的时间)
+f = (x ** 2 for x in range(1, 1000))
+print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间
+print(f)
+for val in f:
+    print(val)
+    
+Python中还有另外一种定义生成器的方式，就是通过yield关键字将一个普通函数改造成生成器函数。
+下面的代码演示了如何实现一个生成斐波拉切数列的生成器。所谓斐波拉切数列可以通过下面递归的方法来进行定义：
+
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        yield a
+
+
+def main():
+    for val in fib(20):
+        print(val)
+
+
+if __name__ == '__main__':
+    main()
+    
+# 定义元组
+t = ('骆昊', 38, True, '四川成都')
+print(t)
+# 获取元组中的元素
+print(t[0])
+print(t[3])
+# 遍历元组中的值
+for member in t:
+    print(member)
+# 重新给元组赋值
+# t[0] = '王大锤'  # TypeError
+# 变量t重新引用了新的元组原来的元组将被垃圾回收
+t = ('王大锤', 20, True, '云南昆明')
+print(t)
+# 将元组转换成列表
+person = list(t)
+print(person)
+# 列表是可以修改它的元素的
+person[0] = '李小龙'
+person[1] = 25
+print(person)
+# 将列表转换成元组
+fruits_list = ['apple', 'banana', 'orange']
+fruits_tuple = tuple(fruits_list)
+print(fruits_tuple)
+'''
